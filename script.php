3679778be97e74482fd9b52dac9019cd29a2cf70
Fede <chiellofederico@gmail.com>
2021-02-26 14:02:57 +0100

    Init

---
+++ script.php
@@ -0,0 +1,18 @@
+<!-- Riscrivere questa pagina del sito google https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte. Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
+
+<!-- PHP -->
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Google Faq</title>
+</head>
+<body>
+
+</body>
+</html>
